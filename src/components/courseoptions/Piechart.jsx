0218334dcff388f30e4a5d57cb0f7f02fa9a0452
Anas <aanu8148@gmail.com>
2025-05-18 17:50:37 +0530

    Fix file extension from .jsx.jsx to .jsx

--- src/components/courseoptions/Piechart.jsx
+++ src/components/courseoptions/Piechart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
-import Overview from "../../components/courseoptions/Overview";
+import Overview from "./Overview";
 
 const PieChart = () => {
     const chartRef = useRef(null);
